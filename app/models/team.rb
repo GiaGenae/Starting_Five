a37ce4777fe12814a360da70d76e591932282ded
Gia Jennings <gjennings.co@gmail.com>
2021-02-25 21:21:03 -0500

    Edit migrations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ActiveRecord::Base
+    has_many :players
+    belongs_to :coach, class_name: "User"
+    has_secure_password
 end
--- db/migrate/20210226004649_create_teams.rb
+++ db/migrate/20210226004649_create_teams.rb
@@ -4,8 +4,7 @@ class CreateTeams < ActiveRecord::Migration[5.2]
       t.text :private_key
       t.text :public_key
       t.string :name
-      t.string :string
-      t.references :coach, foreign_key: [to_table: :user, on_delete: :cascade]
+      t.references :coach, foreign_key: {to_table: :users, on_delete: :cascade}
       t.string :password_digest
       t.string :api_key
       t.text :seed_phrase
